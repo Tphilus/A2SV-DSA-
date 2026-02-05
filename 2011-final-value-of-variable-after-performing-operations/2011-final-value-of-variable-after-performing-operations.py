e677283a0288d5d49dca7c3951d89430b5570906
Theophilus K. Dadzie <98459253+Tphilus@users.noreply.github.com>
2026-02-05 15:28:00 -0800

    Time: 0 ms (100%), Space: 19.4 MB (30.63%) - LeetHub

--- 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.py
+++ 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.py
@@ -1,14 +1,16 @@
 class Solution:
     def finalValueAfterOperations(self, operations: List[str]) -> int:
-        operations_dic = {
+        operation_dic = {
             "++X": 1,
             "X++": 1,
             "--X": -1,
             "X--": -1
         }
 
-        res = 0
+        counts = 0
         for sign in operations:
-            res += operations_dic.get(sign, 0)
+            counts += operation_dic.get(sign, 0)
         
-        return res
+        return counts
+            
+            
