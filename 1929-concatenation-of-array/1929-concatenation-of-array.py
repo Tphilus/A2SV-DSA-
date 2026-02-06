c54e831ab47a9f068920d4973266bbdbeede3a2a
Theophilus K. Dadzie <98459253+Tphilus@users.noreply.github.com>
2026-02-06 22:04:15 +0000

    Time: 0 ms (100%), Space: 19.5 MB (23.2%) - LeetHub

--- 1929-concatenation-of-array/1929-concatenation-of-array.py
+++ 1929-concatenation-of-array/1929-concatenation-of-array.py
@@ -1,4 +1,3 @@
 class Solution:
     def getConcatenation(self, nums: List[int]) -> List[int]:
-        first_arr = second_arr = nums
-        return first_arr + second_arr
+        return nums + nums
