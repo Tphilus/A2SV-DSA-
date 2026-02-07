a4b9ce7300c001a914a071d98807d5356d93ddb2
Theophilus K. Dadzie <98459253+Tphilus@users.noreply.github.com>
2026-02-07 23:14:32 +0000

    Add array left rotation functionality
    Implement the rotateLeft function to perform left rotations on an array.

---
+++ hackerrank/array-left-rotation.py
@@ -0,0 +1,78 @@
+# A  operation on a circular array shifts each of the array's elements  unit to the left. The elements that fall off the left end reappear at the right end. Given an integer , rotate the array that many steps to the left and return the result.
+
+# Example
+
+
+# After  rotations, .
+
+# Function Description
+
+# Complete the  function with the following parameters:
+
+# : the amount to rotate by
+# : the array to rotate
+# Returns
+
+# : the rotated array
+# Input Format
+
+# The first line contains two space-separated integers that denote , the number of integers, and , the number of left rotations to perform.
+# The second line contains  space-separated integers that describe .
+
+# Constraints
+
+# Sample Input
+
+# STDIN      Function
+# -----      --------
+# 5 4         n = 5 d = 4
+# 1 2 3 4 5  arr = [1, 2, 3, 4, 5]
+# Sample Output
+
+# 5 1 2 3 4
+# Explanation
+
+# To perform  left rotations, the array undergoes the following sequence of changes:
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'rotateLeft' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts following parameters:
+#  1. INTEGER d
+#  2. INTEGER_ARRAY arr
+#
+
+def rotateLeft(d, arr):
+    # Write your code here
+    n = len(arr)
+    d = d % n
+    
+    return arr[d:] + arr[:d]
+            
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    d = int(first_multiple_input[1])
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = rotateLeft(d, arr)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
