3d76e7d4c04852d85673cc73ab90bc045db7941c
Theophilus K. Dadzie <98459253+Tphilus@users.noreply.github.com>
2026-02-26 10:13:47 +0000

    Add solution for Minimum Number of Moves to Seat Everyone (trial 1)

---
+++ leetcode/minimum-number-of-moves-to-seat-everyone_trial1.py
@@ -0,0 +1,10 @@
+class Solution:
+    def minMovesToSeat(self, seats: List[int], students: List[int]) -> int:
+        seats.sort()
+        students.sort()
+        
+        moves = 0
+        for i, j in zip(seats, students):
+            moves += abs(i - j)
+        
+        return moves
