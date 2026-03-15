cb192c74c17664a460e8c3f71dd51b6f34ae708d
Theophilus K. Dadzie <98459253+Tphilus@users.noreply.github.com>
2026-03-15 18:03:47 +0000

    Add A_Beautiful_Matrix.py solution
    Implement solution to count moves for beautiful matrix.

---
+++ codeforces/A_Beautiful_Matrix.py
@@ -0,0 +1,47 @@
+# A. Beautiful Matrix
+# time limit per test2 seconds
+# memory limit per test256 megabytes
+# You've got a 5 × 5 matrix, consisting of 24 zeroes and a single number one. Let's index the matrix rows by numbers from 1 to 5 from top to bottom, let's index the matrix columns by numbers from 1 to 5 from left to right. In one move, you are allowed to apply one of the two following transformations to the matrix:
+
+# Swap two neighboring matrix rows, that is, rows with indexes i and i + 1 for some integer i (1 ≤ i < 5).
+# Swap two neighboring matrix columns, that is, columns with indexes j and j + 1 for some integer j (1 ≤ j < 5).
+# You think that a matrix looks beautiful, if the single number one of the matrix is located in its middle (in the cell that is on the intersection of the third row and the third column). Count the minimum number of moves needed to make the matrix beautiful.
+
+# Input
+# The input consists of five lines, each line contains five integers: the j-th integer in the i-th line of the input represents the element of the matrix that is located on the intersection of the i-th row and the j-th column. It is guaranteed that the matrix consists of 24 zeroes and a single number one.
+
+# Output
+# Print a single integer — the minimum number of moves needed to make the matrix beautiful.
+
+# Examples
+# InputCopy
+# 0 0 0 0 0
+# 0 0 0 0 1
+# 0 0 0 0 0
+# 0 0 0 0 0
+# 0 0 0 0 0
+# OutputCopy
+# 3
+# InputCopy
+# 0 0 0 0 0
+# 0 0 0 0 0
+# 0 1 0 0 0
+# 0 0 0 0 0
+# 0 0 0 0 0
+# OutputCopy
+# 1
+
+# ==== CODE ==== 
+target_row = 0
+target_col = 0
+
+for i in range(1,6):
+    row_data = list(map(int, input().split()))
+    
+    if 1 in row_data:
+        target_row = i
+        target_col = row_data.index(1) + 1
+        
+        break
+moves = abs(target_row - 3) + abs(target_col - 3)
+print(moves)
